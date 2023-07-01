d9d9254d2414cdbd76813df973e588a31d1dc5af
Akbar S <54757270+akbartus@users.noreply.github.com>
2023-07-01 16:09:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+def hello_world():
+    return render_template('hello.html')
